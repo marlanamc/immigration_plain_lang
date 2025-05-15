dbac2617e5ca8c58a996506c0ea87055862aea6d
marlanamc <marlana.creed@gmail.com>
2025-05-15 18:43:41 -0400

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/immigration_plain_lang',
+  assetPrefix: '/immigration_plain_lang/',
 }
 
 module.exports = nextConfig 
